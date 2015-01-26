54310c2c58fb413d4f13d8b5f4759be63240c046
Claus Ibsen <claus.ibsen@gmail.com>
2015-01-26 16:32:52 +0100

    (cherry picked from commit 85d6a3c2c24c1ea7917d357e4af2d54c4580fc46)
    Conflicts:
    	hawtio-web/src/main/webapp/lib/camelModel.js

--- hawtio-web/src/test/specs/spec-js/CamelSpec.js
+++ hawtio-web/src/test/specs/spec-js/CamelSpec.js
@@ -1,6 +1,6 @@
 describe("Camel", function () {
 
-  angular.forEach(["endpoint", "from", "to", "bean", "filter", "when", "otherwise"], function (value) {
+  angular.forEach(["from", "to", "bean", "filter", "when", "otherwise"], function (value) {
     it("the type '" + value + "' should be a valid camel pattern type name", function () {
       expect(Camel.isCamelPattern(value)).toEqual(true);
     });
@@ -9,8 +9,8 @@ describe("Camel", function () {
   angular.forEach([
     "",
     "unknownThing",
-    "org.apache.camel.model.language.ExpressionDefinition",
-    "org.apache.camel.model.dataformat.SyslogDataFormat",
+    "get",
+    "syslog",
     "org.apache.camel.model.language.Expression"
   ], function (value) {
     it("the type name '" + value + "' should not be a valid camel pattern type name", function () {
