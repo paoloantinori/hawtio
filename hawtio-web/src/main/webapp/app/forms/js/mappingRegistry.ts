c97343309dbe9ba39f90a13a936cd2e8586f4f65
Claus Ibsen <claus.ibsen@gmail.com>
2015-01-29 10:38:31 +0100

    (cherry picked from commit e0ce89d39612c1c746c296e3390d3313bbf892d8)
    Conflicts:
    	hawtio-web/src/main/webapp/app/forms/js/formHelpers.ts

--- hawtio-web/src/main/webapp/app/forms/js/mappingRegistry.ts
+++ hawtio-web/src/main/webapp/app/forms/js/mappingRegistry.ts
@@ -91,7 +91,7 @@ module Forms {
 
         var controlDiv = Forms.getControlDiv(config);
         controlDiv.append(input);
-        controlDiv.append(Forms.getHelpSpan(config, config, id));
+        controlDiv.append(Forms.getHelpSpan(config, config, id, property));
 
         group.append(controlDiv);
 
