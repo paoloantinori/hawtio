c12ae8e2696b4c07dfbce83afcfd4d90a318fe61
Paolo Antinori <pantinor@redhat.com>
2015-07-28 14:49:07 +0200

    ENTESB-1487 - Fix Maven indexer plugin configuration

--- hawtio-maven-indexer/src/main/java/io/hawt/maven/indexer/MavenIndexerFacade.java
+++ hawtio-maven-indexer/src/main/java/io/hawt/maven/indexer/MavenIndexerFacade.java
@@ -284,6 +284,7 @@ public class MavenIndexerFacade extends MBeanSupport implements MavenIndexerFaca
                     LOG.error("Discarding unsupported URL provided to Maven Indexer Service: {}", url, e );
                 }
             }
+            this.repositories = new String[input.length];
             result.toArray(this.repositories);
         }
     }
