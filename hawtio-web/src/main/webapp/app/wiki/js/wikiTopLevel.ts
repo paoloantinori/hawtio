c01777c9783b59996cd9112323066480f88f3140
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2016-08-15 13:32:18 +0900

    ENTESB-5970 - #2148: RBAC for Fabric - Wiki - configuration buttons
    (cherry picked from commit dcd966710a344d414de9f95d7f0dba37679bd618)
    
    Conflicts:
    	hawtio-web/src/main/webapp/app/wiki/js/wikiTopLevel.ts

--- hawtio-web/src/main/webapp/app/wiki/js/wikiTopLevel.ts
+++ hawtio-web/src/main/webapp/app/wiki/js/wikiTopLevel.ts
@@ -15,6 +15,7 @@ module Wiki {
     $scope.schemaLookupMBean = Fabric.schemaLookupMBean;
     $scope.gitMBean = Git.getGitMBean(workspace);
     $scope.configAdminMBean = Osgi.getHawtioConfigAdminMBean(workspace);
+    $scope.hawtioConfigAdminMBean = $scope.configAdminMBean;
     $scope.defaultVersion = Fabric.getDefaultVersionId(jolokia);
   }]);
 
